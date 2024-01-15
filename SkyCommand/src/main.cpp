5951ee6a9bbc71b4bbda17afdb93733168bb3b9a
Alexei Chetroi <Alexei.Chetroi@outlook.com>
2024-01-14 21:26:28 -0500

    Test LCD Screen

--- SkyCommand/src/main.cpp
+++ SkyCommand/src/main.cpp
@@ -3,6 +3,14 @@
 
 #include <Arduino.h>
 
+#ifdef USE_LCD
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#endif
+
 #include "roaster.h"
 
 uint8_t receiveBuffer[ROASTER_MESSAGE_LENGTH];
@@ -12,6 +20,43 @@ double temp = 0.0;
 
 unsigned long time = 0;
 char CorF = 'F';
+
+#ifdef USE_LCD
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+void testLCD(void);
+void setupLCD(void);
+
+void setupLCD(void) {
+  Serial.print("OLED address: ");
+  Serial.println(SCREEN_ADDRESS);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+      Serial.println(F("SSD1306 allocation failed"));
+      for(;;);
+  } else {
+    Serial.println("Adafruit_SSD1306 initialized successfuly");
+  }
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+}
+
+void testLCD(void) {
+  display.clearDisplay();
+
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F("Hello, world!"));
+
+  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
+  display.println(3.141592);
+  //display.display();
+}
+#else
+#define setupLCD(x)
+#define testLCD(x)
+#endif
+
 
 void setControlChecksum() {
   uint8_t sum = 0;
@@ -234,6 +279,9 @@ void setup() {
   Serial.setTimeout(100);
   pinMode(CONTROLLER_PIN_TX, OUTPUT);
   shutdown();
+
+  setupLCD();
+  testLCD();
 }
 
 void loop() {
