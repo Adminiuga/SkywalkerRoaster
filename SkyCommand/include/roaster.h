3ae40f65a4d6b162f7b0b71381495451a69642e0
Alexei Chetroi <Alexei.Chetroi@outlook.com>
2024-03-01 23:55:22 -0500

    Split logging into separate header

--- SkyCommand/include/roaster.h
+++ SkyCommand/include/roaster.h
@@ -37,19 +37,4 @@
 
 typedef unsigned long ustick_t;
 
-#ifdef __DEBUG__
-#define DEBUG(...) Serial.print(__VA_ARGS__)
-#define DEBUGLN(...) Serial.println(__VA_ARGS__)
-#else
-#define DEBUG(...)
-#define DEBUGLN(...)
-#endif
-#ifdef __WARN__
-#define WARN(...) Serial.print(__VA_ARGS__)
-#define WARNLN(...) Serial.println(__VA_ARGS__)
-#else
-#define WARN(...)
-#define WARNLN(...)
-#endif
-
 #endif  // __ROASTER_H
